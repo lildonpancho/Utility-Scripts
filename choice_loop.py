df5f646249a0b859b10e9b977ef36f5b48a3f9eb
Chris Thompson <csthompson521@gmail.com>
2019-11-26 11:43:57 -0800

    creates interactive choice menu for a user

---
+++ choice_loop.py
@@ -0,0 +1,19 @@
+def choice_loop(choice_prompt, options_list):
+    """Gets the users" choice for service/model to pull.
+
+    Args:
+        choice_prompt (str): Asks the user what service/model they would like to use.
+        options_list (list): A list of options for the user to choose from.
+
+    Returns:
+        str: The service or model chosen by the user.
+    """
+    while True:
+        print(choice_prompt)
+        for n, option in enumerate(options_list, start=1):
+            print(f"{n}) {option}")
+        try:
+            choice = int(input("Enter choice: "))
+            return options_list[choice-1]
+        except Exception:
+            print('Invalid selection. Please try again.\n')
